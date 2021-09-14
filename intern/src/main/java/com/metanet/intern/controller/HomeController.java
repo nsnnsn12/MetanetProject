46b108a3d1a6a4547e93eb8468e35dcc2b2d1eeb
YorkStan <80165832+HanHangGyu@users.noreply.github.com>
2021-09-15 08:40:28 +0900

    타임리프 레이아웃 테스트 코드

--- intern/src/main/java/com/metanet/intern/controller/HomeController.java
+++ intern/src/main/java/com/metanet/intern/controller/HomeController.java
@@ -9,4 +9,34 @@ public class HomeController {
 	public String test() {
 		return "thymeleaf/index";
 	}
+	
+	@RequestMapping("/hello")
+	public String tes2t() {
+		return "thymeleaf/hello";
+	}
+	@RequestMapping("/default")
+	public String tes3t() {
+		return "thymeleaf/default";
+	}
+	
+	@RequestMapping("/topbar")
+	public String frag_top() {
+		return "thymeleaf/topbar";
+	}
+	@RequestMapping("/footer")
+	public String frag_foot() {
+		return "thymeleaf/footer";
+	}
+	@RequestMapping("/head")
+	public String frag_head() {
+		return "thymeleaf/head";
+	}
+	@RequestMapping("/script")
+	public String frag_script() {
+		return "thymeleaf/script";
+	}
+	@RequestMapping("/sidebar")
+	public String frag_side() {
+		return "thymeleaf/sidebar";
+	}
 }
