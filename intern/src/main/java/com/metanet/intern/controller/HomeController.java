b69e268f3aeae8bfffa85b126365b63d9b0a01e2
DESKTOP-A037TC6\user <gkdldshtjdrb@naver.com>
2021-09-16 13:31:45 +0900

    test file delete

--- intern/src/main/java/com/metanet/intern/controller/HomeController.java
+++ intern/src/main/java/com/metanet/intern/controller/HomeController.java
@@ -9,39 +9,9 @@ public class HomeController {
 	@GetMapping(value = {"/","/index"})
 	public String index() {
 		return "thymeleaf/index";
-	}
-	
-	@RequestMapping("/hello")
-	public String test2() {
-		return "thymeleaf/hello";
-	}
-	@RequestMapping("/default")
-	public String test3() {
-		return "thymeleaf/default";
-	}
-	
+	}	
 	@GetMapping("/login")
 	public String longinView() {
 		return "thymeleaf/login";
 	}
-//	@RequestMapping("/topbar")
-//	public String frag_top() {
-//		return "thymeleaf/topbar";
-//	}
-//	@RequestMapping("/footer")
-//	public String frag_foot() {
-//		return "thymeleaf/footer";
-//	}
-//	@RequestMapping("/head")
-//	public String frag_head() {
-//		return "thymeleaf/head";
-//	}
-//	@RequestMapping("/script")
-//	public String frag_script() {
-//		return "thymeleaf/script";
-//	}
-//	@RequestMapping("/sidebar")
-//	public String frag_side() {
-//		return "thymeleaf/sidebar";
-//	}
 }
