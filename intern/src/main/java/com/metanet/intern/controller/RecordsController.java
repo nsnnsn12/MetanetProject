20cf0cb39ce7d16378f3fe338da631325da0f758
jya018 <55496667+jya018@users.noreply.github.com>
2021-09-16 20:18:02 +0900

    학적관리
    1차

--- intern/src/main/java/com/metanet/intern/controller/RecordsController.java
+++ intern/src/main/java/com/metanet/intern/controller/RecordsController.java
@@ -1,5 +1,35 @@
 package com.metanet.intern.controller;
 
-public class RecordsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/records")
+public class RecordsController {
+	
+	//학적 목록조회
+	@GetMapping("list")
+	public String list() {
+		return "/thymeleaf/records/records_list";
+	}
+	
+	//학적 상세조회
+	@GetMapping("detail")
+	public String detail() {
+		return "/thymeleaf/records/records_detail";
+	}
+	
+	//학적 등록
+	@GetMapping("create")
+	public String enroll() {
+		return "/thymeleaf/records/records_create";
+	}
+
+	//학적 수정
+	@GetMapping("modify")
+	public String modify() {
+		return "/thymeleaf/records/records_modify";
+	}
+	
 }
