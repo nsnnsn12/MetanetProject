4c3092d8c4735889b27cd609f43256c269694d23
DESKTOP-A037TC6\user <gkdldshtjdrb@naver.com>
2021-09-16 20:45:20 +0900

    ckeditor 설정

--- intern/src/main/java/com/metanet/intern/config/WebSecurityConfig.java
+++ intern/src/main/java/com/metanet/intern/config/WebSecurityConfig.java
@@ -1,19 +1,23 @@
 package com.metanet.intern.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "/css/**", "/js/**", "/img/**", "/lib/**" };
-	
+
 	//정적 자원에 대해서는 security 설정을 하지 않는다.
 	@Override
 	public void configure(WebSecurity web) throws Exception {
