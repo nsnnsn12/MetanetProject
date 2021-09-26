25666d3d01d0529993293a6a35c6dd14a6fbd58b
DESKTOP-A037TC6\user <gkdlshtjdrb@naver.com>
2021-09-26 19:03:51 +0900

    프로필 기능 구현

--- intern/src/main/java/com/metanet/intern/controller/FileController.java
+++ intern/src/main/java/com/metanet/intern/controller/FileController.java
@@ -1,5 +1,7 @@
 package com.metanet.intern.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.metanet.intern.domain.Manager;
 import com.metanet.intern.domain.PhotoFile;
+import com.metanet.intern.service.ManagerService;
 import com.metanet.intern.service.StorageService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +24,24 @@ public class FileController {
 	@Autowired
 	StorageService storageService;
 	
+	@Autowired
+	ManagerService managerService;
+	
 	@GetMapping("download/{id}")
 	@ResponseBody
 	public ResponseEntity<Resource> serveFile(@PathVariable("id")PhotoFile photoFile) {
 		Resource file = storageService.loadAsResource(photoFile.getSaveFileName());
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	}
+	
+	@GetMapping("profileImage")
+	@ResponseBody
+	public ResponseEntity<Resource> profileImage(Principal principal) {
+		log.info(principal.getName());
+		log.info(managerService.findOne(principal.getName()).toString());
+		Manager manager = managerService.findOne(principal.getName());
+		Resource file = storageService.loadAsResource(manager.getPhoto().getSaveFileName());
 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
 				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
 	}
