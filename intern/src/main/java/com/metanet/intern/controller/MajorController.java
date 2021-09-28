3af417e67ca680d49779e55d3b74e585ae7ac677
jya018 <55496667+jya018@users.noreply.github.com>
2021-09-28 20:59:12 +0900

    전공관리
    검색기능 구현

--- intern/src/main/java/com/metanet/intern/controller/MajorController.java
+++ intern/src/main/java/com/metanet/intern/controller/MajorController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.metanet.intern.domain.Major;
 import com.metanet.intern.domain.Manager;
 import com.metanet.intern.service.MajorService;
+import com.metanet.intern.vo.MajorSearchCondition;
 import com.metanet.intern.vo.Pager;
+import com.metanet.intern.vo.StudentSearchCondition;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,9 +41,8 @@ public class MajorController {
 	}
 	
 	@GetMapping("list")
-	public String list(Pageable pageable, Model model) {
-		Page<Major> page = majorService.getAllList(pageable);
-		model.addAttribute("majorList", page.getContent());
+	public String list(@ModelAttribute("condition") MajorSearchCondition condition, Pageable pageable, Model model) {
+		Page<Major> page = majorService.searchList(pageable, condition);
 		for(Major major : page.getContent()) {
 			log.info(major.getTitle());
 			log.info(major.getCode());
